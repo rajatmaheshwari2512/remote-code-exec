59c2101ab3574a3f94ef3b97720e7d13ee8ab492
rajatmaheshwari2512 <mrajat67@yahoo.com>
2020-12-15 21:58:51 +0530

    Added Input Support for Python and C++

--- routes/codeUpload.js
+++ routes/codeUpload.js
@@ -6,18 +6,33 @@ var router = express.Router();
 var output;
 router.post("/", (req, res, next) => {
   const code = req.body.code;
-  fs.writeFileSync(`input.${languageCode[req.body.langid]}`, code, (err) => {
+  const langid = req.body.langid;
+  const input = req.body.input;
+  fs.writeFileSync(`input.${languageCode[langid]}`, code, (err) => {
     if (err) return console.log(err);
   });
-  if (req.body.langid == 1) {
-    output = cmd.runSync("g++ input.cpp && ./a.out");
+  if (langid == 1) {
+    if (input == null) output = cmd.runSync("g++ input.cpp && ./a.out");
+    else {
+      fs.writeFileSync("input.txt", input, (err) => {
+        if (err) return console.log(err);
+      });
+      output = cmd.runSync("g++ input.cpp && ./a.out <input.txt");
+      cmd.runSync("rm input.txt");
+    }
   } else if (req.body.langid == 2) {
-    output = cmd.runSync("python input.py");
+    if (input == null) output = cmd.runSync("python input.py");
+    else {
+      fs.writeFileSync("input.txt", input, (err) => {
+        if (err) return console.log(err);
+      });
+      output = cmd.runSync("python input.py <input.txt");
+      cmd.runSync("rm input.txt");
+    }
   }
-  console.log(output);
   res.json({ output });
-  cmd.runSync(`rm input.${languageCode[req.body.langid]}`);
-  if (req.body.langid == 1) {
+  cmd.runSync(`rm input.${languageCode[langid]}`);
+  if (langid == 1) {
     cmd.runSync("rm a.out");
   }
   res.end();
