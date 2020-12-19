dfecb0947499f0925cd45a83d1be5f2ad53fffb9
rajatmaheshwari2512 <mrajat67@yahoo.com>
2020-12-19 15:33:11 +0530

    Removed redundant code

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var createError = require("http-errors");
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var codeRouter = require("./routes/codeUpload");
+var cors = require("cors");
 
 var app = express();
 
@@ -11,6 +11,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/codeupload", codeRouter);
 
--- routes/codeUpload.js
+++ routes/codeUpload.js
@@ -2,23 +2,35 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 var express = require("express");
 var fs = require("fs");
+
 var { languageCode } = require("../shared/languageCode");
 var { pythonList } = require("../shared/blacklist");
+
 var router = express.Router();
+router.get("/", (req, res, next) => {
+  res.json({ APIWorking: true });
+});
 router.post("/", (req, res, next) => {
   const code = req.body.code;
   const langid = req.body.langid;
   const input = req.body.input;
-  console.log(langid, input);
+  console.log(typeof input);
   fs.writeFile(`input.${languageCode[langid]}`, code, (err) => {
     if (err) res.json({ error: err });
     if (langid == 1) {
-      if (!input) {
-        exec("g++ input.cpp && ./a.out", { timeout: 5000, maxBuffer: 50000 })
+      fs.writeFile("input.txt", input, (err) => {
+        if (err) res.json({ error: err });
+        exec("g++ input.cpp && ./a.out <input.txt", {
+          timeout: 5000,
+          maxBuffer: 50000,
+        })
           .then((result) => {
             res.json(result);
             exec("rm input.cpp && rm a.out").then((resp) =>
               console.log("CPP File Deleted")
+            );
+            exec("rm input.txt").then((resp) =>
+              console.log("Input CPP Deleted")
             );
           })
           .catch((err) => {
@@ -26,72 +38,34 @@ router.post("/", (req, res, next) => {
             exec("rm input.cpp && rm a.out").then((resp) =>
               console.log("CPP File Deleted")
             );
+            exec("rm input.txt").then((resp) =>
+              console.log("Input CPP Deleted")
+            );
           });
-      } else {
-        fs.writeFile("input.txt", input, (err) => {
-          if (err) res.json({ error: err });
-          exec("g++ input.cpp && ./a.out <input.txt", {
-            timeout: 5000,
-            maxBuffer: 50000,
-          })
-            .then((result) => {
-              res.json(result);
-              exec("rm input.cpp && rm a.out").then((resp) =>
-                console.log("CPP File Deleted")
-              );
-              exec("rm input.txt").then((resp) =>
-                console.log("Input CPP Deleted")
-              );
-            })
-            .catch((err) => {
-              res.json(err);
-              exec("rm input.cpp && rm a.out").then((resp) =>
-                console.log("CPP File Deleted")
-              );
-              exec("rm input.txt").then((resp) =>
-                console.log("Input CPP Deleted")
-              );
-            });
-        });
-      }
+      });
     } else if (langid == 2) {
-      if (!input) {
-        exec("python input.py", { timeout: 10000, maxBuffer: 100000 })
+      console.log("Input");
+      fs.writeFile("input.txt", input, (err) => {
+        if (err) res.json({ error: err });
+        exec("python input.py <input.txt", {
+          timeout: 10000,
+          maxBuffer: 100000,
+        })
           .then((result) => {
             res.json(result);
             exec("rm input.py").then((resp) => console.log("PY File Deleted"));
+            exec("rm input.txt").then((resp) =>
+              console.log("Input PY Deleted")
+            );
           })
           .catch((err) => {
             res.json(err);
             exec("rm input.py").then((resp) => console.log("PY File Deleted"));
+            exec("rm input.txt").then((resp) =>
+              console.log("Input PY Deleted")
+            );
           });
-      } else {
-        fs.writeFile("input.txt", input, (err) => {
-          if (err) res.json({ error: err });
-          exec("python input.py <input.txt", {
-            timeout: 10000,
-            maxBuffer: 100000,
-          })
-            .then((result) => {
-              res.json(result);
-              exec("rm input.py").then((resp) =>
-                console.log("PY File Deleted")
-              );
-              exec("rm input.txt").then((resp) =>
-                console.log("Input PY Deleted")
-              );
-            })
-            .catch((err) => {
-              res.json(err);
-              exec("rm input.py").then((resp) =>
-                console.log("PY File Deleted")
-              );
-              exec("rm input.txt").then((resp) =>
-                console.log("Input PY Deleted")
-              );
-            });
-        });
-      }
+      });
     }
   });
 });
